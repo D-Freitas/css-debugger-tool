2a10398a060e1d7c0cdf1d87cdd521d6f941f113
d-freitas <davi.israel01@gmail.com>
2022-06-17 14:42:54 -0300

    feat: on-demand processing

--- src/classes/line-delimiters.js
+++ src/classes/line-delimiters.js
@@ -18,40 +18,47 @@ class LineDelimiters {
   }
 
   disable () {
-    Array.from(this.elements).forEach(element => {
+    for (const element of this.#bringPageElements()) {
       element.onmouseover = element.onmouseleave = null
       const isElementShadow = element.style.boxShadow !== null
       const elementHasLine = this.colorsRgbaWithLine.includes(element.style.boxShadow)
       if (isElementShadow && elementHasLine) { element.style.boxShadow = null }
-    })
+    }
     this.states.runDebugger.forEach((state, i) => {
       this.states.runDebugger[i] = false
     })
   }
 
-  #bringPageElements () {
-    this.elements = document.body.querySelectorAll('*')
+  * #bringPageElements () {
+    const elementsFromBody = document.body.querySelectorAll('*')
+    for (const element of elementsFromBody) {
+      yield element
+    }
   }
 
   #bringColors () {
-    const elements = Array.from(this.elements)
-    let targetIndex = 0
-    elements.forEach((element, index) => {
-      if (targetIndex >= this.colors.length) { targetIndex = 0 }
-      this.colors[index] = this.colors[targetIndex]
-      targetIndex++
-    })
+    const elements = this.#bringPageElements()
+    let index = 0
+    let colorIndex = 0
+
+    while (typeof elements.next().value !== 'undefined') {
+      if (colorIndex >= this.colors.length) colorIndex = 0
+      this.colors[index] = this.colors[colorIndex]
+      index++
+      colorIndex++
+    }
   }
 
   #insertEventsToElements () {
-    Array.from(this.elements).forEach((element, i) => {
-      element.onmouseover = () => {
-        this.events.addLinesDelimiters(element, this.dataConverter.convertHexToRgba(this.colors[i]))
-      }
-      element.onmouseleave = () => {
-        this.events.removeLinesDelmiters(element, this.dataConverter.convertHexToRgba(this.colors[i]))
-      }
-    })
+    let i = 0
+
+    for (const element of this.#bringPageElements()) {
+      const lineColor = this.colors[i]
+      const hexColor = this.dataConverter.convertHexToRgba(lineColor)
+      element.onmouseover = () => this.events.addLinesDelimiters(element, hexColor)
+      element.onmouseleave = () => this.events.removeLinesDelmiters(element, hexColor)
+      i++
+    }
   }
 }
 
