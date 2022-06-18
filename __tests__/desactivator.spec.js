cefbf6c376e4e7d5e9625f8ce0dad29cf4e41d8b
d-freitas <davi.israel01@gmail.com>
2022-06-17 22:17:39 -0300

    test: ensure debugger is desactivated after hotkeys are pressed again

---
+++ __tests__/desactivator.spec.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-undef */
+/* eslint-disable quotes */
+let isInitiated = true
+let pressedKeys = ']cdt'
+
+describe('deactivator', () => {
+  it("should be remove 't' in pressedKeys string if isInitiated is true and pressedKeys is ']cdt'", () => {
+    function remover (inputtedKey) {
+      if (isInitiated && pressedKeys === ']cdt') {
+        pressedKeys = pressedKeys.slice(0, -1)
+      }
+      return pressedKeys
+    }
+
+    const KEY_PRESSED_MOCK = ']'
+    const keyInserted = remover(KEY_PRESSED_MOCK)
+    expect(keyInserted).toBe(']cd')
+  })
+
+  it("should be remove 'd' in pressedKeys string if isInitiated is true and pressedKeys is ']cd'", () => {
+    function remover (inputtedKey) {
+      if (isInitiated && pressedKeys === ']cd') {
+        pressedKeys = pressedKeys.slice(0, -1)
+      }
+      return pressedKeys
+    }
+
+    const KEY_PRESSED_MOCK = 't'
+    const keyInserted = remover(KEY_PRESSED_MOCK)
+    expect(keyInserted).toBe(']c')
+  })
+
+  it("should be remove 'c' in pressedKeys string if isInitiated is true and pressedKeys is ']c'", () => {
+    function remover (inputtedKey) {
+      if (isInitiated && pressedKeys === ']c') {
+        pressedKeys = pressedKeys.slice(0, -1)
+      }
+      return pressedKeys
+    }
+
+    const KEY_PRESSED_MOCK = 'd'
+    const keyInserted = remover(KEY_PRESSED_MOCK)
+    expect(keyInserted).toBe(']')
+  })
+
+  it("should be remove ']' in pressedKeys string if isInitiated is true and pressedKeys is ']'", () => {
+    function remover (inputtedKey) {
+      if (isInitiated && pressedKeys === ']') {
+        pressedKeys = pressedKeys.slice(0, -1)
+      }
+      return pressedKeys
+    }
+
+    const KEY_PRESSED_MOCK = 'c'
+    const keyInserted = remover(KEY_PRESSED_MOCK)
+    expect(keyInserted).toBe('')
+  })
+
+  it("should be change modify initializer state to false if pressedKeys is empty string", () => {
+    if (pressedKeys === '') {
+      isInitiated = false
+      expect(isInitiated).toBe(false)
+    }
+  })
+})
